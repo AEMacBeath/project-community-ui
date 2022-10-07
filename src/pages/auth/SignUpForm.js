df67cf725f821b97e26dc0239a6f891065ba0e29
AEMacBeath <alicemacbeath94@gmail.com>
2022-10-07 21:12:55 +0000

    Created SignUp form and linked it to project-community-api.

---
+++ src/pages/auth/SignUpForm.js
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import { Col, Container, Form, Row, Button, Alert } from "react-bootstrap";
+
+import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import { Link, useHistory } from "react-router-dom";
+import axios from "axios";
+
+const SignUpForm = () => {
+  const [signUpData, setSignUpData] = useState({
+    username: "",
+    password1: "",
+    password2: "",
+  });
+
+  const { username, password1, password2 } = signUpData;
+
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setSignUpData({
+      ...signUpData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/registration/", signUpData);
+      history.push("/signin");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
+  return (
+    <Row>
+      <Col>
+        <Container className={styles.Container}>
+          <h2>Sign up form</h2>
+          <p>Please complete the below form to create an account</p>
+          <Form className={styles.Form} onSubmit={handleSubmit}>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.username?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
+            <Form.Group controlId="password1">
+              <Form.Label className="d-none">Password1</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password1"
+                value={password1}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.password1?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
+            <Form.Group controlId="password2">
+              <Form.Label className="d-none">Re-enter Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Re-enter Password"
+                name="password2"
+                value={password2}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.password2?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
+            <Button className={btnStyles.Btn} type="submit">
+              Sign up
+            </Button>
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert key={idx} variant="warning" className="mt-3">
+                {message}
+              </Alert>
+            ))}
+          </Form>
+          <Link className={styles.Link} to="/signin">
+            Already a user? Sign in here.
+          </Link>
+        </Container>
+      </Col>
+    </Row>
+  );
+};
+
+export default SignUpForm;
