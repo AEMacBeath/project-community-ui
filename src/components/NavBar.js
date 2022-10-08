f46d9fea1a46d060aa4cd72699ee35538ab51a4b
AEMacBeath <alicemacbeath94@gmail.com>
2022-10-08 10:25:44 +0000

    Added useClickOutsideToggle hook to close burger.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,22 +2,27 @@ import axios from "axios";
 import React from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser, useSetCurrentUser } from "../contexts/currentUserContext";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../contexts/currentUserContext";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 import styles from "../styles/NavBar.module.css";
 import Avatar from "./Avatar";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
-  const setCurrentUser = useSetCurrentUser()
+  const setCurrentUser = useSetCurrentUser();
+  const {expanded, setExpanded, ref} = useClickOutsideToggle();
 
   const handleSignOut = async () => {
     try {
-      await axios.post('dj-rest-auth/logout/');
+      await axios.post("dj-rest-auth/logout/");
       setCurrentUser(null);
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   const addUserSubmit = (
     <>
@@ -26,7 +31,11 @@ const NavBar = () => {
         className={styles.NavLink}
         activeClassName={styles.Active}
       >
-        <Avatar src={currentUser?.profile_image} text={currentUser?.username} height={40} />
+        <Avatar
+          src={currentUser?.profile_image}
+          text={currentUser?.username}
+          height={40}
+        />
       </NavLink>
       <NavLink
         to="/observsations/create"
@@ -84,13 +93,22 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar
+      className={styles.NavBar}
+      expanded={expanded}
+      expand="md"
+      fixed="top"
+    >
       <Container>
         <NavLink to="/">
           <Navbar.Brand>Project Community</Navbar.Brand>
         </NavLink>
         {currentUser && addUserSubmit}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          ref={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav"
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-right">
             <NavLink
